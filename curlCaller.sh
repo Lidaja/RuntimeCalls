4ba44f196e33e5a16a4975eda04897071f688025
Liam Jackson <lijax@hotmail.com>
2016-07-22 23:53:48 +0200

    Adding more parameters to curlCaller

--- curlCaller.sh
+++ curlCaller.sh
@@ -1,4 +1,7 @@
-curlId=$(curl --unix-socket /var/run/system-docker.sock -s -H "Content-Type: application/json" -X POST -d  '{"AttachStdin": false, "AttachStdout": true, "AttachStderr": true, "Tty": true, "Cmd": ["python", "/tmp/Api-Invokers/createCluster.py", "10.10.30.235", "10.10.30.236" ] }' http:/containers/console/exec | jq '.Id' | tr -d '\"')
+ip = $1
+vip = $2
+num = $3
+curlId=$(curl --unix-socket /var/run/system-docker.sock -s -H "Content-Type: application/json" -X POST -d  '{"AttachStdin": false, "AttachStdout": true, "AttachStderr": true, "Tty": true, "Cmd": ["python", "/tmp/Api-Invokers/createCluster.py", $ip, $vip, $num ] }' http:/containers/console/exec | jq '.Id' | tr -d '\"')
 command="curl --unix-socket /var/run/system-docker.sock -s -H \"Content-Type: application/json\" -X POST -d '{\"Detach\": false, \"Tty\": true }' http:/exec/$curlId/start"
 #echo $command
 result=$(eval $command)
