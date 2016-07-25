92ca1eb921b2a0c6ca00040d793733657d000c3a
Liam Jackson <lijax@hotmail.com>
2016-07-25 23:54:42 +0200

    So close

--- curlCaller.sh
+++ curlCaller.sh
@@ -1,9 +1,10 @@
-ip = $1
-vip = $2
-num = $3
-curlId=$(curl --unix-socket /var/run/system-docker.sock -s -H "Content-Type: application/json" -X POST -d  '{"AttachStdin": false, "AttachStdout": true, "AttachStderr": true, "Tty": true, "Cmd": ["python", "/tmp/Api-Invokers/createCluster.py", $ip, $vip, $num ] }' http:/containers/console/exec | jq '.Id' | tr -d '\"')
-command="curl --unix-socket /var/run/system-docker.sock -s -H \"Content-Type: application/json\" -X POST -d '{\"Detach\": false, \"Tty\": true }' http:/exec/$curlId/start"
-#echo $command
+#p=$1
+#params=($p)
+#ip=${params[0]}
+#vip=${params[1]}
+#num=${params[2]}
+command="curl --unix-socket /var/run/system-docker.sock -H \"Content-Type: application/json\" -X POST -d '{\"AttachStdin\": false, \"AttachStdout\": true, \"AttachStderr\": true, \"Tty\": false, \"Cmd\": [ \"python\", \"/tmp/Api-Invokers/createCluster.py\", \"10.10.30.235\", \"10.10.30.234\" ] }' http:/containers/console/exec | jq '.Id' | tr -d '\"'"
+exec_id=$(eval $command)
+command="curl --unix-socket /var/run/system-docker.sock -H \"Content-Type: application/json\" -X POST -d '{\"Detach\": false, \"Tty\": true }' http:/exec/$exec_id/start"
 result=$(eval $command)
 echo $result
-
--- invokeData.java
+++ invokeData.java
@@ -3,8 +3,9 @@ import java.io.*;
 public class invokeData {
 	public static void main(String args[]){
 		try{
+			//System.out.println("test");
 			Process p = Runtime.getRuntime().exec("./curlCaller.sh");
-			printProcess.printData(p);
+			//printProcess.printData(p);
 		}catch(IOException e){
 			System.out.println("IOException");
 		}
