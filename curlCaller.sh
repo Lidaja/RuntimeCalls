2f486f74090af84c00815e52ab79d63a9f459712
Liam Jackson <lijax@hotmail.com>
2016-07-29 21:39:57 +0200

    Https properly works

--- curlCaller.sh
+++ curlCaller.sh
@@ -2,7 +2,7 @@ touch /RuntimeCalls/command.txt
 chmod 777 /RuntimeCalls/command.txt
 touch /RuntimeCalls/command2.txt
 chmod 777 /RuntimeCalls/command2.txt
-command="curl --unix-socket /var/run/system-docker.sock -H \"Content-Type: application/json\" -X POST -d '{\"AttachStdin\": false, \"AttachStdout\": true, \"AttachStderr\": true, \"Tty\": false, \"Cmd\": [ \"python\", \"/tmp/Api-Invokers/createCluster.py\", \"10.10.30.235\", \"10.10.30.234\" ] }' http:/containers/console/exec | jq '.Id' | tr -d '\"'"
+Command="curl --unix-socket /var/run/system-docker.sock -H \"Content-Type: application/json\" -X POST -d '{\"AttachStdin\": false, \"AttachStdout\": true, \"AttachStderr\": true, \"Tty\": false, \"Cmd\": [ \"python\", \"/tmp/Api-Invokers/createCluster.py\", \"10.10.30.235\", \"10.10.30.234\" ] }' http:/containers/console/exec | jq '.Id' | tr -d '\"'"
 command2="ls -al"
 exec_id=$(eval $command)
 exec_id2=$(eval $command2)
--- test.java
+++ test.java
@@ -2,52 +2,62 @@ import java.io.*;
 import java.net.URL;
 import java.net.HttpURLConnection;
 import java.net.InetAddress;
+//import java.lang.StringBuffer;
 public class test {
 	public static void main(String args[]){
 		try{
 			
-			URL url = new URL("http://172.0.0.1:2375/info/");
-			//URL url = new URL("http://localhost:23");
-	                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-			conn.setDoOutput(true);
-        	        conn.setRequestMethod( "POST" );
-                	//conn.addRequestProperty("Content-Type", "application/json");
-	                //conn.connect();
-        	        //byte[] outputBytes = "{\"AttachStdin\": false, \"AttachStdout\": true, \"AttachStderr\": true, \"Tty\": false, \"Cmd\": [ \"ls\",\"-al\"]}".getBytes("UTF-8");
-                	//OutputStream os = conn.getOutputStream();
-	                //os.write(outputBytes);
-                	//os.close();
-			InputStream is = conn.getInputStream();
-			System.out.println(getStringFromInputStream(is));
-			is.close();
+			URL url_id = new URL("http://10.10.30.235:2375/containers/console/exec");
+	                HttpURLConnection conn_id = (HttpURLConnection)url_id.openConnection();
+			conn_id.setDoOutput(true);
+        	        conn_id.setRequestMethod( "POST" );
+                	conn_id.addRequestProperty("Content-Type", "application/json"); 
+			OutputStream os = conn_id.getOutputStream();
+			OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+        	        //osw.write("{\"AttachStdin\": false, \"AttachStdout\": true, \"AttachStderr\": true, \"Tty\": false, \"Cmd\": [ \"ls\",\"-al\"]}");
+        	        osw.write("{\"AttachStdin\": false, \"AttachStdout\": true, \"AttachStderr\": true, \"Tty\": false, \"Cmd\": [ \"python\", \"/tmp/Api-Invokers/createCluster.py\", \"10.10.30.235\", \"10.10.30.234\" ]}");
+			osw.flush();
+			osw.close();
+					
+			StringBuilder sb = getStringBuilder(conn_id);
+			String ID = sb.toString().substring(7,sb.toString().length()-3);
+			System.out.println(ID);
+			
+			URL url_start = new URL("http://10.10.30.235:2375/exec/"+ID+"/start");
+			
+			HttpURLConnection conn_start = (HttpURLConnection)url_start.openConnection();
+			conn_start.setDoOutput(true);
+                        conn_start.setRequestMethod( "POST" );
+                        conn_start.addRequestProperty("Content-Type", "application/json");
+                        OutputStream os_start = conn_start.getOutputStream();
+                        OutputStreamWriter osw_start = new OutputStreamWriter(os_start, "UTF-8");
+                        osw_start.write("{\"Detach\": false, \"Tty\": true}");
+                        osw_start.flush();
+                        osw_start.close();
+			System.out.println(conn_start.getResponseCode());
+			StringBuilder sb_start = getStringBuilder(conn_start);
+			System.out.println(sb_start.toString());
+
 		}catch(IOException e){
-			System.out.println(e.getMessage());
-		}
-	}
-
-	public static String getStringFromInputStream(InputStream is){
-		BufferedReader br = null;
-		StringBuilder sb = new StringBuilder();
-
-		String line;
-
-		try{
-			br = new BufferedReader(new InputStreamReader(is));
-			while ((line = br.readLine()) != null){
-				sb.append(line);
-			}
-		}
-		catch (IOException e){
+			System.out.println("ERROR1");
 			e.printStackTrace();
-		} finally {
-			if (br != null){
-				try {
-					br.close();
-				} catch (IOException e){
-					e.printStackTrace();
-				}
-			}
 		}
-		return sb.toString();
 	}
+
+	public static StringBuilder getStringBuilder(HttpURLConnection con){
+		StringBuilder sb = new StringBuilder();
+		try{
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+			String line = null;
+			while ((line = br.readLine()) != null){
+				sb.append(line+"\n");
+			}
+			br.close();
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+		return sb;
+	}
+
 }
