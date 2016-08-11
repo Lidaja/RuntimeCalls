d3d47929bf343863358e36baaa605d8caf0c9129
Liam Jackson <lijax@hotmail.com>
2016-08-11 17:08:17 +0200

    new test

--- daemonTest.java
+++ daemonTest.java
@@ -1,52 +1,37 @@
 import java.io.*;
+import java.lang.InterruptedException;
 import java.net.URL;
 import java.net.HttpURLConnection;
 import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 //import java.lang.StringBuffer;
 public class daemonTest {
 	public static void main(String args[]){
+	
+		String ip = "10.10.30.235";
+		String vip = "10.10.30.234";
+		ArrayList<String> params = new ArrayList<String>();
+		params.add("bash");
+		params.add("/tmp/Api-Invokers/writer");
+		params.add(ip+" "+vip);
+		params.add("/tmp/cluster.txt");
+		System.out.println(Arrays.toString(params.toArray()));
+		System.out.println(runCommand(params));
+		/*
 		try{
-			
-			URL url_id = new URL("http://10.10.30.235:2375/containers/console/exec");
-	                HttpURLConnection conn_id = (HttpURLConnection)url_id.openConnection();
-			conn_id.setDoOutput(true);
-        	        conn_id.setRequestMethod( "POST" );
-                	conn_id.addRequestProperty("Content-Type", "application/json"); 
-			OutputStream os = conn_id.getOutputStream();
-			OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
-			String ip = "10.10.30.235";
-			String vip = "10.10.30.234";
-			String message = "{\"AttachStdin\": false, \"AttachStdout\": true, \"AttachStderr\": true, \"Tty\": false, \"Cmd\": [ \"bash\", \"/tmp/writer\", \""+ip+" "+vip+"\",\"/tmp/cluster.txt\" ]}";
-
-			System.out.println(message);
-			
-			osw.write(message);
-			osw.flush();
-			osw.close();
-					
-			StringBuilder sb = getStringBuilder(conn_id);
-			String ID = sb.toString().substring(7,sb.toString().length()-3);
-			System.out.println(ID);
-			
-			URL url_start = new URL("http://10.10.30.235:2375/exec/"+ID+"/start");
-			
-			HttpURLConnection conn_start = (HttpURLConnection)url_start.openConnection();
-			conn_start.setDoOutput(true);
-                        conn_start.setRequestMethod( "POST" );
-                        conn_start.addRequestProperty("Content-Type", "application/json");
-                        OutputStream os_start = conn_start.getOutputStream();
-                        OutputStreamWriter osw_start = new OutputStreamWriter(os_start, "UTF-8");
-                        osw_start.write("{\"Detach\": false, \"Tty\": true}");
-                        osw_start.flush();
-                        osw_start.close();
-			System.out.println(conn_start.getResponseCode());
-			StringBuilder sb_start = getStringBuilder(conn_start);
-			System.out.println(sb_start.toString());
-
-		}catch(IOException e){
-			System.out.println("ERROR1");
-			e.printStackTrace();
+			Thread.sleep(3000);
+		} catch(InterruptedException I){
+			System.out.println("I");
 		}
+		params.clear();
+		params.add("sudo");
+		params.add("rm");
+		params.add("/tmp/cluster.txt");
+		System.out.println(Arrays.toString(params.toArray()));
+		System.out.println(runCommand(params));*/
+							
 	}
 
 	public static StringBuilder getStringBuilder(HttpURLConnection con){
@@ -63,6 +48,50 @@ public class daemonTest {
 			e.printStackTrace();
 		}
 		return sb;
+	}
+	public static String runCommand(ArrayList params){
+		StringBuilder sb_start = null;
+		try{
+	
+			URL url = new URL("http://10.10.30.235:2375/containers/console/exec");
+		        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+		        conn.setDoOutput(true);
+                	conn.setRequestMethod( "POST" );
+	                conn.addRequestProperty("Content-Type", "application/json"); 
+		        OutputStream os = conn.getOutputStream();
+	        	OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+			String message = "{\"AttachStdin\": false, \"AttachStdout\": true, \"AttachStderr\": true, \"Tty\": false, \"Cmd\": [ ";
+			for (int i = 0;i<params.size()-1;i++){
+				message = message + "\"" + params.get(i) + "\",";
+			}
+			message = message + "\"" + params.get(params.size()-1) + "\" ]}";
+			osw.write(message);
+			osw.flush();
+			osw.close();
+			System.out.println(message);
+			StringBuilder sb = getStringBuilder(conn);
+			String ID = sb.toString().substring(7,sb.toString().length()-3);
+
+
+			URL url_start = new URL("http://10.10.30.235:2375/exec/"+ID+"/start");
+		
+			HttpURLConnection conn_start = (HttpURLConnection)url_start.openConnection();
+			conn_start.setDoOutput(true);
+                	conn_start.setRequestMethod( "POST" );
+        	        conn_start.addRequestProperty("Content-Type", "application/json");
+	                OutputStream os_start = conn_start.getOutputStream();
+                	OutputStreamWriter osw_start = new OutputStreamWriter(os_start, "UTF-8");
+        	        osw_start.write("{\"Detach\": false, \"Tty\": true}");
+	                osw_start.flush();
+                	osw_start.close();
+			System.out.println(conn_start.getResponseCode());
+			sb_start = getStringBuilder(conn_start);
+		
+		} catch (IOException e){
+			System.out.println("error");
+		}
+		return (sb_start.toString());
+
 	}
 
 }
